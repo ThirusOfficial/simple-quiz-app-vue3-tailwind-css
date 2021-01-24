f190a17de37bf6852ec2f28db79b8687277a575e
Thirus <thirusofficial@gmail.com>
2021-01-24 12:36:12 +0530

    Simple Quiz App Completed

---
+++ public/scripts.js
@@ -0,0 +1,52 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            index: 0,
+            selectedAnswer: '',
+            correctAnswer: 0,
+            wrongAnswer: 0,
+            count: 3,
+            questions: [
+                {
+                    question: "Rolex is a company that specializes in what type of product?",
+                    answers: {a: 'Bags', b: 'Watches', c: 'Shoes', d: 'Laptops'},
+                    correctAnswer: 'b'
+                },
+                {
+                    question: "When did Facebook launch?",
+                    answers: {a: '2005', b: '2008', c: '2003', d: '2004'},
+                    correctAnswer: 'd'
+                },
+                {
+                    question: "Albert Einstein had trouble with mathematics when he was in school",
+                    answers: {a: 'True', b: 'False'},
+                    correctAnswer: 'b'
+                },
+            ]
+        }
+    },
+    methods: {
+        answered(e) {
+            this.selectedAnswer = e.target.value
+            if(this.selectedAnswer == this.questions[this.index]['correctAnswer'])
+                this.correctAnswer++
+            else
+                this.wrongAnswer++
+        },
+        nextQuestion() {
+            this.index++
+            this.selectedAnswer = ''
+        },
+        showResults() {
+            this.index++
+        },
+        resetQuiz() {
+            this.index = 0
+            this.selectedAnswer = ''
+            this.correctAnswer = 0
+            this.wrongAnswer = 0
+        }
+    }
+})
+
+app.mount('#app')
